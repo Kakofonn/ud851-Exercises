5130996ab44853fba438ea1491a691e77a163c88
Anatolii <kakofonn@gmail.com>
2017-11-20 21:32:31 +0200

    Exercise 4b.02 complete

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -22,6 +22,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import static android.net.Uri.*;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -48,12 +50,12 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickOpenAddressButton(View v) {
-        // TODO (5) Store an address in a String
+        String address = "Mäkitie 6, Ristiina";
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("geo").path("0,0").query(address);
+        Uri addressUri = builder.build();
 
-        // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
-
-        // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        showMap(addressUri);
     }
 
     /**
@@ -71,9 +73,8 @@ public class MainActivity extends AppCompatActivity {
      * similar to what I've done above. You can view a list of implicit Intents on the Common
      * Intents page from the developer documentation.
      *
-     * @see <http://developer.android.com/guide/components/intents-common.html/>
-     *
      * @param v Button that was clicked.
+     * @see <http://developer.android.com/guide/components/intents-common.html/>
      */
     public void createYourOwn(View v) {
         Toast.makeText(this,
@@ -93,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
          * We wanted to demonstrate the Uri.parse method because its usage occurs frequently. You
          * could have just as easily passed in a Uri as the parameter of this method.
          */
-        Uri webpage = Uri.parse(url);
+        Uri webpage = parse(url);
 
         /*
          * Here, we create the Intent with the action of ACTION_VIEW. This action allows the user
@@ -111,14 +112,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    // TODO (1) Create a method called showMap with a Uri as the single parameter
-    // Do steps 2 - 4 within the showMap method
-        // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
-
-        // TODO (3) Set the data of the Intent to the Uri passed into this method
-
-        // TODO (4) Verify that this Intent can be launched and then call startActivity
-
+    private void showMap(Uri uri) {
+        // Do steps 2 - 4 within the showMap method
+        Intent showMapIntent = new Intent(Intent.ACTION_VIEW);
+        showMapIntent.setData(uri);
+        if (showMapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(showMapIntent);
+        }
+    }
 
 }
